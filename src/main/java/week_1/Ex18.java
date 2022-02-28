0d896d0a38902541513c86f6df7cc6bdf27cd5a8
Elena Piaggio <43441336+elenapiaggio@users.noreply.github.com>
2022-02-28 16:52:36 +0100

    add ex18

---
+++ src/main/java/week_1/Ex18.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ *  Escribir una función para invertir manualmente un array pasado por parámetro.
+ *  Es decir, dada la entrada {0,1,2,4,8} se modificaría esta como [8,4,2,1,0]. 
+ *  Una posible solución es recorrer el array desde 0 hasta su mitad, 
+ *  e ir intercambiando el primer elemento con el último, el segundo con el penúltimo y así sucesivamente.
+ *  
+ * @author epiaggio
+ *
+ */
+public class Ex18 {
+	
+	public static void main(String[] args) {
+		
+		int array[]  = {0,1,2,4,8};
+		
+		invertirArray(array);
+		for(int i: array) {
+			System.out.println(i);
+		}
+	}
+
+	private static int[] invertirArray(int[] array) {
+		
+		int aux = 0;
+		
+		for (int i = 0; i < array.length/2; i++) {
+			aux = array[i]; 
+			array[i] = array[array.length - 1 - i];
+			array[array.length - 1 - i] = aux;
+		}
+		
+		return array;
+	}
+
+}
