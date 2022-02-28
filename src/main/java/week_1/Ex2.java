0c4d13f4a7bd684f756e6631165596f6c894412f
Elena Piaggio <43441336+elenapiaggio@users.noreply.github.com>
2022-02-28 18:34:51 +0100

    add ex2

---
+++ src/main/java/week_1/Ex2.java
@@ -0,0 +1,21 @@
+package week_1;
+
+/**
+ * Crea una función que recibe una cadena de texto y devuelve el número de vocales.
+ * @author UOC
+ *
+ */
+public class Ex2 {
+	
+	public static void main(String[] args) {
+		
+		String someString = "hola";
+		int totalVowels = givemeVowels(someString);
+		System.out.println("El String tiene: " + totalVowels + " words.");
+		
+	}
+
+	public static int givemeVowels(String theString) {
+		return 1;
+	}
+}
